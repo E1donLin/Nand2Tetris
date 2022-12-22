c6d206372f9cef62620952d6a86509230d7a2d72
Eldon Lin <yl4039@rit.edu>
2022-12-22 00:07:45 -0500

    Project 4 completed

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,48 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+  // screenend = 24575
+  @24575
+  D = A
+  @screenend
+  M = D
+
+  // current = current index on screen
+  @SCREEN
+  D = A
+  @current
+  M = D
+  
+(LOOP)
+  // if (KBD > 0) goto FILL else goto CLEAR
+  @KBD
+  D = M
+  @FILL
+  D;JGT
+  @CLEAR
+  0;JMP
+
+(FILL)
+  // set cufrent pixel to black
+  @current
+  D = M
+  A = M
+  M = -1
+
+  @current
+  M = D + 1
+
+  @LOOP
+  0;JMP
+
+(CLEAR)
+  // set current pixel to white
+  @current
+  D = M
+  A = M
+  M = 0
+
+  @current
+  M = D - 1
+  @LOOP
+  0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,30 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// R2 = 0
+  @R2
+  M = 0
+
+(LOOP)
+  // if (R1 == 0) goto END
+  @R1
+  D = M
+  @END
+  D;JEQ
+
+  //R2 = R2 + R0
+  @R0
+  D = M
+  @R2
+  M = D + M
+  
+  //R1 = R1 - 1
+  @R1
+  M = M - 1
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
